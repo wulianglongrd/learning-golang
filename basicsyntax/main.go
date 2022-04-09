1199a1ee86617f2e97e7837ff3b25d69fec3213a
longwuliang <longwuliang@didiglobal.com>
2022-04-09 18:19:50 +0800

    feat: 常量和变量

---
+++ basicsyntax/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	variableSyntax()
+	constantSyntax();
+}
+
+
+func variableSyntax() {
+	// var name type
+	var a = 1
+	b := 2
+	x, y := 100, "abc"
+	fmt.Println(a, b, x, y)
+}
+
+func constantSyntax() {
+	// const identifier [type] = value
+	const a string = "aaa"
+	const b = "bbb"
+	fmt.Println(a, b)
+}
