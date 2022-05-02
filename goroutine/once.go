d93be574d91592a06dfe91b88f9237f531549a52
wulianglongrd <wulianglongrd@gmail.com>
2022-05-02 12:53:29 +0800

    feat: add sync.once example

--- goroutine/once.go
+++ goroutine/once.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	once := sync.Once{}
+	go once.Do(runOnce)
+	go once.Do(runOnce)
+	go once.Do(runOnce)
+
+	time.Sleep(time.Second)
+}
+
+func runOnce() {
+	fmt.Println("run...")
+}
