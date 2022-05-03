264a97fac34d9847a0131fce7fc2287c81392300
wulianglongrd <wulianglongrd@gmail.com>
2022-05-03 18:05:26 +0800

    feat: add recover example

--- error/recover.go
+++ error/recover.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+// https://go.dev/blog/defer-panic-and-recover
+// https://gobyexample.com/recover
+func main() {
+	controller()
+}
+
+func controller() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recovered, err:", r)
+		}
+	}()
+
+	panic("a problem")
+	fmt.Println("after panic")
 }
