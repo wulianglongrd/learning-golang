57f17e3c8a5664cf47a22b5538f827a919cf5bbe
wulianglongrd <wulianglongrd@gmail.com>
2022-05-03 15:53:03 +0800

    style: go fmt

--- goroutine/dataRace.go
+++ goroutine/dataRace.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
 
-//$ go run -race dataRace.go
-//main goroutine... 3
-//==================
-//WARNING: DATA RACE
-//Write at 0x00c00001a0f8 by goroutine 7:
-//main.main.func1()
-///Users/logx/go/src/learning-golang/goroutine/dataRace.go:11 +0x34
+// $ go run -race dataRace.go
+// main goroutine... 3
+// ==================
+// WARNING: DATA RACE
+// Write at 0x00c00001a0f8 by goroutine 7:
+// main.main.func1()
+// /Users/logx/go/src/github.com/wulianglongrd/learning-golang/goroutine/dataRace.go:31 +0x34
 //
-//Previous write at 0x00c00001a0f8 by main goroutine:
-//main.main()
-///Users/logx/go/src/learning-golang/goroutine/dataRace.go:15 +0xb8
+// Previous write at 0x00c00001a0f8 by main goroutine:
+// main.main()
+// /Users/logx/go/src/github.com/wulianglongrd/learning-golang/goroutine/dataRace.go:35 +0xb8
 //
-//Goroutine 7 (running) created at:
-//main.main()
-///Users/logx/go/src/learning-golang/goroutine/dataRace.go:10 +0xae
-//==================
-//goroutine... 2
-//Found 1 data race(s)
-//exit status 66
+// Goroutine 7 (running) created at:
+// main.main()
+// /Users/logx/go/src/github.com/wulianglongrd/learning-golang/goroutine/dataRace.go:30 +0xae
+// ==================
+// goroutine... 2
+// Found 1 data race(s)
+// exit status 66
 
-func main()  {
-  a := 1
-  go func() {
-    a = 2
-    fmt.Println("goroutine...", a)
-  }()
+func main() {
+	a := 1
+	go func() {
+		a = 2
+		fmt.Println("goroutine...", a)
+	}()
 
-  a = 3
-  time.Sleep(1)
-  fmt.Println("main goroutine...", a)
+	a = 3
+	time.Sleep(1)
+	fmt.Println("main goroutine...", a)
 }
