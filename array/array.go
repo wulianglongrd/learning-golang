46a502d9a77dd16f6265d28654b43604c9780f53
wulianglongrd <wulianglongrd@gmail.com>
2022-05-03 14:17:43 +0800

    fix: fix func name

--- array/array.go
+++ array/array.go
@@ -4,35 +4,35 @@ import "fmt"
 
 func main() {
 	arrayDefine()
-	arrayLenth()
+	arrayLength()
 	arrayLoop()
 }
 
 func arrayDefine() {
-	// define array
+	// 1. define array
 	// var name [size]type
 	var foo [3]int
-	fmt.Println(foo)
+	fmt.Println(foo) // [0 0 0]
 
 	foo = [3]int{1, 2, 3}
-	fmt.Println(foo)
+	fmt.Println(foo) // [1 2 3]
 
-	// define array with init value
+	// 2. define array with init value
 	// name := [size]type{...}
 	bar := [3]int{1, 2, 3}
-	fmt.Println(bar)
+	fmt.Println(bar) // [1 2 3]
 
-	// define without size
+	// 3. define without size
 	// name := [...]type{...}
 	names := [...]string{"foo", "bar"}
-	fmt.Println(names)
+	fmt.Println(names) // [foo bar]
 
-	// define with key
+	// 4. define with key
 	f := [...]int{0: 1, 3: 2, 5: 3}
-	fmt.Println(f)
+	fmt.Println(f) // [1 0 0 2 0 3]
 }
 
-func arrayLenth() {
+func arrayLength() {
 	fmt.Println("---------------")
 
 	var foo [3]int
@@ -42,7 +42,7 @@ func arrayLenth() {
 	fmt.Println(len(bar)) // 3
 
 	names := [...]string{"foo", "bar"}
-	fmt.Println(len(names)) // 3
+	fmt.Println(len(names)) // 2
 
 	f := [...]int{0: 1, 3: 2, 5: 3}
 	fmt.Println(len(f)) // 6
