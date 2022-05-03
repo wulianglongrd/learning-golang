2adc4840d0358be54949dd178959641952ee8944
wulianglongrd <wulianglongrd@gmail.com>
2022-05-03 14:41:25 +0800

    refactor: refactor slice example

--- array/slice.go
+++ array/slice.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	sliceDefine()
 	sliceFromArray()
-  sliceMakeAndNew()
+	sliceMakeAndNew()
 }
 
 func sliceDefine() {
@@ -34,16 +34,25 @@ func sliceFromArray() {
 	// slice和数组共享数据，slice和数组修改都会相互反应
 	s1[0] = 100
 	arr[2] = 200
-	fmt.Println(s1) // [100 200]
+	fmt.Println(s1)  // [100 200]
 	fmt.Println(arr) // [1 100 200 4 5]
 }
 
 // New 返回指针地址 more @see func description
-// make 返回第一个元素，可预设内存空间，避免未来的内存拷贝 more @see func description
-func sliceMakeAndNew()  {
-  s1 := new([]int)
-  fmt.Println(s1) // &[]
+// make 返回slice的零值，可预设内存空间，避免未来的内存拷贝 more @see func description
+func sliceMakeAndNew() {
+	s1 := new([]int)
+	fmt.Println(s1) // &[]
 
-  s2 := make([]int, 3)
-  fmt.Println(s2) // [0 0 0]
+	s2 := make([]int, 3) // 创建一个基于底层数组为 [3]int 的 len和cap都为3的slice
+	fmt.Println(s2)      // [0 0 0]
+	fmt.Println(len(s2)) // 3
+	fmt.Println(cap(s2)) // 3
+
+	fmt.Println()
+
+	s3 := make([]int, 1, 3) // 创建一个基于底层数组为 [3]int 的len为1, cap为3的slice
+	fmt.Println(s3)         // [0]
+	fmt.Println(len(s3))    // 1
+	fmt.Println(cap(s3))    // 3
 }
--- array/sliceVolume.go
+++ array/sliceVolume.go
@@ -2,24 +2,47 @@ package main
 
 import "fmt"
 
-// len 反应的是切片的长度
-// cap 反应的是切版的底层数组从“切片的索引”开始的底层数组的长度
+// slice 体积
 func main() {
-	makeSlice()
-	arrSplit()
+	noInit()
+	makeInit()
+	arrInit()
 }
 
-func makeSlice() {
-	var s = make([]int, 3, 5)
-	fmt.Println(s, len(s), cap(s)) // [0 0 0] 3 5
-
-	var s2 []int
-	fmt.Println(s2, len(s2), cap(s2)) // [] 0 0
+func noInit() {
+	var s []int
+	fmt.Println(s, len(s), cap(s)) // [] 0 0
 }
 
-func arrSplit() {
-	arr := [...]int{1, 2, 3, 4, 5}
+func makeInit() {
+	fmt.Println()
+	s1 := make([]int, 3)
+	fmt.Printf("s1 len: %d\n", len(s1)) // 3
+	fmt.Printf("s1 cap: %d\n", cap(s1)) // 3
+	fmt.Printf("s1 val: %d\n", s1)      // [0 0 0]
 
-	s := arr[1:3] // 1,3 为数组 arr的下标，1含 3不含
-	fmt.Println(s, len(s), cap(s)) // [2 3] 2 4
+	fmt.Println()
+
+	s2 := make([]int, 1, 3)
+	fmt.Printf("s2 len: %d\n", len(s2)) // 1
+	fmt.Printf("s2 cap: %d\n", cap(s2)) // 3
+	fmt.Printf("s2 val: %d\n", s2)      // [0]
+}
+
+func arrInit() {
+	fmt.Println()
+
+	s1 := []int{0, 1, 2, 3, 4, 5, 6, 7}
+	s2 := s1[3:6]
+
+	fmt.Println(s2) // [3 4 5]
+
+	// s1[3:6] 是数学区间表示法，实际表示[3,6) 左闭右开（含3，不含6）
+	// len: slice的区间长度，即窗口可见元素个数。len = 6 - 3
+	fmt.Printf("s2 len: %d\n", len(s2)) // 3
+
+	// cap: 透过这个窗口，最多可以看到的底层数组中元素的个数。窗口无法向左扩展。
+	// cap = 底层数组的长度 - 切片的起始位值。cap = 8 - 3
+	// 注意：数组的起始索引是0
+	fmt.Printf("s2 cap: %d\n", cap(s2)) // 5
 }
