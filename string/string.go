4c77b8129f6af7ef16b528bb9d5fec77ffd6133b
wulianglongrd <wulianglongrd@gmail.com>
2022-05-03 14:45:12 +0800

    style: go fmt

--- string/string.go
+++ string/string.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-  "fmt"
-  "unicode/utf8"
+	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-  stringBasic()
-  strLen()
+	stringBasic()
+	strLen()
 }
 
 // 长文本可使用 ``, 内含双引号不需要转义
 func stringBasic() {
-  str := "Hello world!"
-  for i := 0; i < len(str); i++ {
-    fmt.Printf("%d = %c\n", str[i], str[i])
-  }
+	str := "Hello world!"
+	for i := 0; i < len(str); i++ {
+		fmt.Printf("%d = %c\n", str[i], str[i])
+	}
 }
 
 // 字节长度：和编码无关，用 len(str) 获取
 // strings.Xxx
-func strLen()  {
-  s := "你好"
-  fmt.Println(len(s)) // 6
-  fmt.Println(utf8.RuneCountInString(s)) // 2
-  fmt.Println(utf8.RuneCountInString("你好ab")) // 4
+func strLen() {
+	s := "你好"
+	fmt.Println(len(s))                         // 6
+	fmt.Println(utf8.RuneCountInString(s))      // 2 统计字符个数
+	fmt.Println(utf8.RuneCountInString("你好ab")) // 4
 }
